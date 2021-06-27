051bbd97c22928d3810dab7acb70c43c1248941e
Tran Sinh <sinhtran@MacBook-Pro-2.local>
2021-06-27 21:39:02 +0700

    Hien- thay doi thong bao

---
+++ src/containers/MessageContainer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+import Message from "../components/Message";
+
+
+class MessageContainer  extends React.Component {
+    render() {
+        var message = this.props.message;
+        return (
+            <Message message={message}/>
+
+        )
+    };
+};
+MessageContainer.propTypes = {
+    message: PropTypes.string.isRequired
+}
+
+const mapStateToProps = (state) => {
+    return {
+        message: state.message
+    }
+}
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(MessageContainer);
